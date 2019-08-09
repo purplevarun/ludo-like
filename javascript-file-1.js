f38ecb00fa7173c5a14fc7d047df0aec3e99b6ea
Varun Kedia <purplevarun@gmail.com>
2019-08-10 00:57:21 +0530

    dice img added

--- javascript-file-1.js
+++ javascript-file-1.js
@@ -1,13 +1,22 @@
 document.getElementById('new-game').addEventListener('click',function(){
+//---------------------------------BACKGROUND MUSIC CODE -----------------------------------
 	document.getElementById('gaana').play();
 	document.getElementById('new-game').textContent='Stop Game.';
 	document.getElementById('new-game').onclick=function(){
 		location.reload();	
 	};
-});
-document.getElementById('new-game').addEventListener('mouseover',function(){
-	document.getElementById('new-game').style.backgroundColor='green';
-});
-document.getElementById('new-game').addEventListener('mouseout',function(){
-	document.getElementById('new-game').style.backgroundColor='white';
+
+//--------------------------------NEW GAME MOUSE-OVER CODE----------------------------------
+	document.getElementById('new-game').addEventListener('mouseover',function(){
+		document.getElementById('new-game').style.backgroundColor='green';
+	});
+	document.getElementById('new-game').addEventListener('mouseout',function(){
+		document.getElementById('new-game').style.backgroundColor='white';
+	});
+//---------------------------------ROLL DICE ONCLICK CODE------------------------------------
+	var rollbtn = document.getElementById('roll');
+	rollbtn.onclick=function(){
+		var value = Math.floor(Math.random()*6)+1;
+		alert(value);
+	}
 });
