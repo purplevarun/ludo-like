e837fdb4def3bb38bdb90bf48409dc600b8cd52c
Varun Kedia <purplevarun@gmail.com>
2019-08-10 12:38:16 +0530

    new gameAudio and viewport removed

--- javascript-file-1.js
+++ javascript-file-1.js
@@ -1,4 +1,4 @@
-var value,user_score=0,rival_score=0,rival_value;
+var isPlaying=true,value,user_score=0,rival_score=0,rival_value;
 //-----------------------------------BEFORE GAME STARTS--------------------------------------
 document.getElementById('roll').style.display='none';
 document.getElementById('dice-pic1').style.display='none';
@@ -15,60 +15,70 @@ document.getElementById('new-game').addEventListener('mouseover',function(){
 //----------------------------------------AFTER GAME STARTS--------------------------------
 document.getElementById('new-game').addEventListener('click',function(){
 //---------------------------------BACKGROUND MUSIC CODE -----------------------------------
-	document.getElementById('gaana').play();
-	document.getElementById('new-game').textContent='Stop Game.';
-	document.getElementById('new-game').onclick=function(){
-		location.reload();	
-	};	
-//---------------------------------ROLL DICE ONCLICK CODE------------------------------------
-	var rollbtn = document.getElementById('roll');
-	rollbtn.onmouseover=function(){
-		rollbtn.style.backgroundColor='darkcyan';
-	};
-	rollbtn.onmouseout=function(){
-		rollbtn.style.backgroundColor='beige';
-	};
-	rollbtn.addEventListener('click',user);
-	rollbtn.addEventListener('click',rival);
-	
-//--------------------------------------------------------------------------------------------
+	if(isPlaying==true){
+		document.getElementById('gaana').play();
+		document.getElementById('new-game').textContent='Stop Game.';
+		document.getElementById('new-game').onclick=function(){
+			location.reload();	
+		};	
+	//---------------------------------ROLL DICE ONCLICK CODE------------------------------------
+		var rollbtn = document.getElementById('roll');
+		rollbtn.onmouseover=function(){
+			rollbtn.style.backgroundColor='darkcyan';
+		};
+		rollbtn.onmouseout=function(){
+			rollbtn.style.backgroundColor='beige';
+		};
+		
+			rollbtn.addEventListener('click',user);
+			rollbtn.addEventListener('click',rival);
+		
+	//--------------------------------------------------------------------------------------------
 
-	document.getElementById('roll').style.display='block';
-	document.getElementById('dice-pic1').style.display='block';
-	document.getElementById('dice-pic2').style.display='block';
-//--------------------------------------------------------------------------------------------
-	function user(){
-		value = Math.floor(Math.random()*6)+1;
-		document.getElementById('dice-img1').src='game-images/dice-'+value+'.png';
-		user_score=user_score + value;
-		document.getElementById('score-0').innerHTML=user_score;
-		if(user_score>=40){
-			if(user_score>rival_score){
-				document.getElementById('score-0').style.fontSize='30px';
-				document.getElementById('score-0').textContent='YOU WON';
-			}
-			else{
-				document.getElementById('score-1').style.fontSize='30px';
-				document.getElementById('score-1').textContent='RIVAL WON';
-			}
-		}	
-	};
-	
-	function rival(){
-		rival_value = Math.floor(Math.random()*6)+1;
-		document.getElementById('dice-img2').src='game-images/dice-'+rival_value+'.png';
-		rival_score=rival_score + rival_value;
-		document.getElementById('score-1').innerHTML=rival_score;
-		if(rival_score>=40){
-			if(rival_score>user_score){
-				document.getElementById('score-1').style.fontSize='30px';
-				document.getElementById('score-1').textContent='RIVAL WON';
-			}
-			else{
-				document.getElementById('score-0').style.fontSize='30px';
-				document.getElementById('score-0').textContent='YOU WON';
-			}
-		}	
-	};
+		document.getElementById('roll').style.display='block';
+		document.getElementById('dice-pic1').style.display='block';
+		document.getElementById('dice-pic2').style.display='block';
+	//--------------------------------------------------------------------------------------------
+		function user(){
+			value = Math.floor(Math.random()*6)+1;
+			document.getElementById('dice-img1').src='game-images/dice-'+value+'.png';
+			user_score=user_score + value;
+			document.getElementById('score-0').innerHTML=user_score;
+			if(user_score>=40){
+				if(user_score>rival_score){
+					document.getElementById('score-0').style.fontSize='30px';
+					document.getElementById('score-0').textContent='YOU WON';
+					endGame();
+				}
+				else{
+					document.getElementById('score-1').style.fontSize='30px';
+					document.getElementById('score-1').textContent='RIVAL WON';
+					endGame();
+				}
+			}	
+		};
 
+		function rival(){
+			rival_value = Math.floor(Math.random()*6)+1;
+			document.getElementById('dice-img2').src='game-images/dice-'+rival_value+'.png';
+			rival_score=rival_score + rival_value;
+			document.getElementById('score-1').innerHTML=rival_score;
+			if(rival_score>=40){
+				if(rival_score>user_score){
+					document.getElementById('score-1').style.fontSize='30px';
+					document.getElementById('score-1').textContent='RIVAL WON';
+					endGame();
+				}
+				else{
+					document.getElementById('score-0').style.fontSize='30px';
+					document.getElementById('score-0').textContent='YOU WON';
+					endGame();
+				}
+			}	
+		};
+		function endGame(){
+			document.getElementById('roll').style.display='none';
+			document.getElementById('gaana').pause();
+		}
+	}
 });
